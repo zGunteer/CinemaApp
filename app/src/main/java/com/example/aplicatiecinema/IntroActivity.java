4f44a4f3988ec5e9685002ba0acf85d595ef7fff
kais230 <kais.2003@yahoo.com>
2024-05-01 13:47:13 +0300

    mesaj

---
+++ app/src/main/java/com/example/aplicatiecinema/IntroActivity.java
@@ -0,0 +1,31 @@
+package com.example.aplicatiecinema;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_intro);
+
+        Button getInBtn=findViewById(R.id.getInBtn);
+        getInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this, LoginActivity.class));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/aplicatiecinema/LoginActivity.java
@@ -0,0 +1,56 @@
+package com.example.aplicatiecinema;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText userEdt, passEdt;
+    private Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        initView();
+    }
+
+    private void initView()
+    {
+        userEdt=findViewById(R.id.editTextText);
+        passEdt=findViewById(R.id.editTextPassword);
+        loginBtn=findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(userEdt.getText().toString().isEmpty() || passEdt.getText().toString().isEmpty())
+                {
+                    Toast.makeText(LoginActivity.this,"Please fill your Username and Password", Toast.LENGTH_SHORT).show();
+
+                }
+                else if(userEdt.getText().toString().equals("test") && passEdt.getText().toString().equals("test"))
+                {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, "Incorrect Username or Password", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+}
